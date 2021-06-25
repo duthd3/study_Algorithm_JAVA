459b3b3c0d2a92c305ed84315910500fbf9d38de
YeoSong <duthd3@naver.com>
2021-06-25 22:59:09 +0900

    Update _12_numRec3.java

--- AlgorithmBase/_12_numRec3.java
+++ AlgorithmBase/_12_numRec3.java
@@ -4,12 +4,14 @@ public class _12_numRec3 {
 	public static void main(String[] args) {
 		
 		int n = 4;
+		int num=1;
 		
 		int[][] arr = new int[n][n];
 		
 		for(int i=0; i<arr.length; i++) {
 			for(int j=0; j<arr[i].length; j++) {
-				arr[i][j] = (i+1)+4*(j);
+				arr[j][i] = num;
+				num++;
 			}
 		}
 		
