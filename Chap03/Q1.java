7b0761aeff739129ff5d119c907812e7199482d0
YeoSong <duthd3@naver.com>
2021-08-16 23:58:27 +0900

    Create Q1.java

---
+++ Chap03/Q1.java
@@ -0,0 +1,42 @@
+package chap03;
+
+import java.util.Scanner;
+
+public class Q1 {
+	static int seqSearchSen(int[] a, int n, int key) {
+		
+		int i = 0;
+		a[n] = key;
+		
+		for(i=0; i<n; i++) {
+			if(a[i] == key)
+				break;
+		}
+		
+		
+		return i == n? -1 : i;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num + 1];
+		
+		for(int i=0; i<num; i++) {
+			System.out.print("x[" + i + "]:");
+			x[i] = sc.nextInt();
+		}
+		System.out.print("검색할 값:");
+		int ky = sc.nextInt();
+		
+		int idx = seqSearchSen(x, num ,ky);
+		
+		if(idx == -1)
+			System.out.println("그 값의 요소가 없습니다.");
+		else
+			System.out.println(ky+ "은(는) x[" + idx + "]에 있습니다." );
+	}
+
+}
