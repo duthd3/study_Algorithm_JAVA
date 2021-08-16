5fba614b5191a11abb65347d3b2a0ee40900ffc3
YeoSong <duthd3@naver.com>
2021-08-17 00:02:09 +0900

    Create Q6.java

---
+++ Chap03/Q6.java
@@ -0,0 +1,38 @@
+package chap03;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q6 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		
+		System.out.print("x[0]: ");
+		x[0] = sc.nextInt();
+		
+		for(int i=1; i<num; i++) {
+			do {
+				System.out.print("x[" + i + "]:");
+				x[i] = sc.nextInt();
+			}while(x[i] <x[i-1]);
+		}
+		
+		System.out.print("검색할 값:");
+		int ky = sc.nextInt();
+		
+		int idx = Arrays.binarySearch(x, ky);
+		
+		if(idx<0)
+			System.out.println(-idx-1);
+		else
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+	}
+
+}
