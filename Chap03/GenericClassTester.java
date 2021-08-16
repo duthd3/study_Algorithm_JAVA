45b85eb93155201c5142920b035236bdc49dab17
YeoSong <duthd3@naver.com>
2021-08-17 00:39:29 +0900

    Create GenericClassTester.java

---
+++ Chap03/GenericClassTester.java
@@ -0,0 +1,22 @@
+package chap03;
+
+public class GenericClassTester {
+	static class GenericClass<T>{
+		private T xyz;
+		GenericClass(T t){
+			this.xyz = t;
+		}
+		T getXyz() {
+			return xyz;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenericClass<String> s = new GenericClass<String>("ABC");
+		GenericClass<Integer> n = new GenericClass<Integer>(15);
+		
+		System.out.println(s.getXyz());
+		System.out.println(n.getXyz());
+	}
+
+}
